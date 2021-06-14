f9cac8658c042485f7d97d263153a37c10baf14c
Vixtor61 <00198117@uca.edu.sv>
2021-06-14 14:08:06 -0600

    implement multiple Ns

--- multiIntegral.py
+++ multiIntegral.py
@@ -1,29 +1,31 @@
 #author Vixtor61
 import math
 
-def f(*args):
+def f1(*args):
+    return  args[0]
+
+def f2(*args):
+    return  math.e**(args[1] /args[0])
+
+def f3(*args):
     return  2*args[1]*math.sin(args[0]) + (math.cos(args[0]))**2
 
 def f4(*args):
-    return  math.e**(args[1] /args[0])
-
-def f2(*args):
-    return  args[0]
-
-def f3(*args):
     return args[0]**2 + args[1]**2 + args[2]
 
-def simpson(fun,limits):
+def simpson(fun,limits,Ns):
     niter = len(limits)/2
-    return simpNested(fun,4,0,niter,limits)
+    return simpNested(fun,Ns,0,niter,limits)
 
 
-def simpNested(fun,n,i,nIter,limits,*args):
-    nhalf = int(n/2)
+def simpNested(fun,Ns,i,nIter,limits,*args):
+    #print(type(Ns))
+    currentN = Ns[i]
+    nhalf = int(currentN/2)
     iterSum = 0 
     limA = limits[2*i](*args) 
     limB = limits[2*i+1](*args) 
-    currentH =( (limB  - limA) / n)
+    currentH =( (limB  - limA) / currentN)
 
     if(i == nIter-1):
         firstSum= 0
@@ -45,26 +47,32 @@ def simpNested(fun,n,i,nIter,limits,*args):
         firstSum= 0
         for j in range(1,nhalf):
             arg= limA +((2*j))*currentH
-            firstSum += 2* simpNested(fun,n,i,nIter,limits,*args,arg) 
+            firstSum += 2* simpNested(fun,Ns,i,nIter,limits,*args,arg) 
 
         secondSum= 0
         for j in range(1,nhalf+1):
             arg= limA +((2*j -1))*currentH
-            secondSum += 4 *  simpNested(fun,n,i,nIter,limits,*args,arg) 
+            secondSum += 4 *  simpNested(fun,Ns,i,nIter,limits,*args,arg) 
         
-        iterSum = simpNested(fun,n,i ,nIter,limits,*args,limA) + firstSum + secondSum + simpNested(fun,n,i,nIter,limits,*args,limB)
+        iterSum = simpNested(fun,Ns,i ,nIter,limits,*args,limA) + firstSum + secondSum + simpNested(fun,Ns,i,nIter,limits,*args,limB)
         return (currentH/3)*iterSum
         
 
 def main():
-    limits = [lambda *x: 0 , lambda *x: math.pi/4 , lambda *x: math.sin(x[0]) ,lambda *x: math.cos(x[0])]
-    limits4 = [lambda *x: 0.1 , lambda *x: 0.5, lambda *x: x[0] **3 ,lambda *x: x[0]** 2]
-    limits2 = [lambda *x: 0 , lambda *x: 4]
-    limits3 = [lambda *x: 2.0 , lambda *x: 4.0 , lambda *x: x[0] - 1.0 ,lambda *x: x[0] +6.0 , lambda *x: -2.0,lambda *x: 4.0 + x[1]**2.0 ]
+    limits = [lambda *x: 0 , lambda *x: 4]
+    limits2 = [lambda *x: 0.1 , lambda *x: 0.5, lambda *x: x[0] **3 ,lambda *x: x[0]** 2]
+    limits3 = [lambda *x: 0 , lambda *x: math.pi/4 , lambda *x: math.sin(x[0]) ,lambda *x: math.cos(x[0])]
+    limits4 = [
+            lambda *x: 2.0 ,                    lambda *x: 4.0 ,
+            lambda *x: x[0] - 1.0 ,             lambda *x: x[0] +6.0 ,
+            lambda *x: -2.0,                    lambda *x: 4.0 + x[1]**2.0 
+            ]
 
-    print("res: " + str(simpson(f,limits)))
-    print("res: " + str(simpson(f2,limits2)))
-    print("res: " + str(simpson(f3,limits3)))
-    print("res: " + str(simpson(f4,limits4)))
+    narray = [10,10,10]
+    #Test Functions
+    print("res: " + str(simpson(f1,limits,narray)))
+    print("res: " + str(simpson(f2,limits2,narray)))
+    print("res: " + str(simpson(f3,limits3,narray)))
+    print("res: " + str(simpson(f4,limits4,narray)))
 main()
 
